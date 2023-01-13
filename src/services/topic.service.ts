0cfc35e9fd9990cbaf35faceab312f51ca9ce452
patrickbyan <patrickbyann@gmail.com>
2023-01-13 11:07:22 +0700

    create store data for caching

--- src/services/topic.service.ts
+++ src/services/topic.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from "@angular/core"
 import * as moment from 'moment';
+import { Topics } from "src/types/topic";
+
+interface LatestData {
+    1: Array<Topics>,
+    2: Array<Topics>,
+    3: Array<Topics>,
+    4: Array<Topics>,
+}
+
+interface Store {
+    latestId: number,
+    latestData: LatestData
+}
 
 @Injectable({ providedIn: 'root' })
 export class TopicService {
+    private store: Store = {
+        latestId: 0,
+        latestData: {
+            1: [],
+            2: [],
+            3: [],
+            4: []
+        }
+    }
+
     private API_URL = 'https://hacker-news.firebaseio.com/v0'
     private async get (url: string) {
         try {
@@ -28,19 +51,52 @@ export class TopicService {
         
         return timestamp.fromNow()
     }
+
+    private removeStore() {
+        this.store = {
+            latestId: 0,
+            latestData: {
+                1: [],
+                2: [],
+                3: [],
+                4: []
+            }
+        }
+    }
+
+    private createStore (id: number, page: number, payload: Array<Topics> = []) {
+        this.store = {
+            latestId: id,
+            latestData: {
+                ...this.store.latestData,
+                [page]: payload
+            }
+        }
+    }
     
     async getTopics(page = 1, size = 5) {
         const offset = (page - 1) * size
 
-
         const fetchTopicIds = await this.get('/askstories.json')
         
         if(fetchTopicIds.err) {
-           return [] 
+            this.removeStore()
+            return [] 
+        }
+
+        const firstId = fetchTopicIds.data[0]
+
+        if(firstId === this.store.latestId) {
+            const { latestData } = this.store
+            
+
+            if(latestData[page as keyof LatestData].length) {
+                return latestData[page as keyof LatestData]
+            }
         }
 
         const topicIds = fetchTopicIds.data.splice(offset, size)
-        
+
         let error = false
         const topics = await Promise.all(
             topicIds.map(async (id: number) => {
@@ -59,8 +115,11 @@ export class TopicService {
         )
 
         if(error) {
+            this.removeStore()
             return []
         }
+
+        this.createStore(firstId, page, topics)
 
         return topics
     }
